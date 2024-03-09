355ffe0eed9be9e157760c0b158d11d705fc232a
ComfortPrince <comfort.prince.hluyo@gmail.com>
2024-03-09 10:58:13 +0200

    Completed structuring and styling the header section.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ module.exports = {
     extend: {
     	colors: {
     		'dark-blue' : 'hsl(209, 23%, 22%)', 				// (Dark Mode Elements)
-			'very-dark-blue' : 'hsl(207, 26%, 17%)',			// (Dark Mode Background)
-			'very-dark-blue' : 'hsl(200, 15%, 8%)',				// (Light Mode Text)
+			'very-dark-blue-dark-mode' : 'hsl(207, 26%, 17%)',			// (Dark Mode Background)
+			'very-dark-blue-light-mode' : 'hsl(200, 15%, 8%)',				// (Light Mode Text)
 			'dark-gray' : 'hsl(0, 0%, 52%)',					// (Light Mode Input)
 			'very-light-gray' : 'hsl(0, 0%, 98%)',				// (Light Mode Background)
 			'white' : 'hsl(0, 0%, 100%)',						// (Dark Mode Text & Light Mode Elements)
